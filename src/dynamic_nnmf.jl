fb44d7b4a903a072f7a3221de5ec1780e483c405
Michael Chung <36006769+MCHChung@users.noreply.github.com>
2026-04-25 13:52:56 -0500

    Add files via upload

--- src/dynamic_nnmf.jl
+++ src/dynamic_nnmf.jl
@@ -88,14 +88,14 @@ function update_A!(Acat, H, V; niter=1)
 end
 
 function dynamic_nmf(
-    X::Matrix,
-    I;
-    J=1,
-    niter=100,
-    M=50,
-    q=0.15,
-    seed=0
-)
+        X::Matrix,
+        I;
+        J=1,
+        niter=100,
+        M=50,
+        q=0.15,
+        seed=0
+    )
     Random.seed!(seed)
 
     K, T = size(X)
@@ -293,7 +293,7 @@ function dynamic_nmf(
     q=0.15,
     seed=0,
     iter = 100
-)
+    )
     Random.seed!(seed)
 
     K, T, E = size(X)
@@ -334,3 +334,180 @@ function dynamic_nmf(
     return W, Hs, A
 end
 
+# ======== UPDATE MATRICES =========
+
+function infer_H_fixedW(
+    Xnew::Matrix,
+    W,
+    A;
+    niter=50,
+    M=0,
+    q=0.15
+    )
+    K, T = size(Xnew)
+    I = size(W,2)
+    J = length(A)
+
+    # --- PLCA scaling ---
+    X = copy(Xnew)
+    X ./= sum(X, dims=1) .+ EPS
+    X .*= 1000.0
+
+    # --- Initialize H ---
+    H = rand(I,T)
+    normalize_columns!(H)
+
+    for r in 1:niter
+        for t in 1:T
+            η = r > M ? predict_h(A, H, t).^q : ones(I)
+            update_h(X[:,t], W, view(H,:,t), η)
+        end
+    end
+
+    return H
+end
+
+function update_A_from_H!(A, H)
+    I, T = size(H)
+    J = length(A)
+
+    V = zeros(I*J, T)
+    for t in 1:T, j in 1:J
+        t-j ≥ 1 && (V[(j-1)*I+1:j*I, t] .= H[:,t-j])
+    end
+
+    Acat = hcat(A...)
+    update_A!(Acat, H, V)
+
+    for j in 1:J
+        A[j] .= Acat[:, (j-1)*I+1:j*I]
+    end
+end
+
+function update_model_matrix!(
+    Xnew,
+    W,
+    A;
+    infer_iters=50,
+    update_A=true
+    )
+
+    Hnew = infer_H_fixedW(Xnew, W, A; niter=infer_iters)
+
+    if update_A
+        update_A_from_H!(A, Hnew)
+    end
+
+    return Hnew, A
+end
+
+function infer_Hs_fixedW(
+    Xnew::Array{<:Real,3},
+    W,
+    A;
+    niter=50,
+    M=0,
+    q=0.15
+    )
+    K, T, E = size(Xnew)
+    I = size(W,2)
+
+    # --- PLCA scaling ---
+    X = copy(Xnew)
+    for e in 1:E, t in 1:T
+        X[:,t,e] ./= sum(X[:,t,e]) + EPS
+        X[:,t,e] .*= 1000
+    end
+
+    Hs = [normalize_columns!(rand(I,T)) for _ in 1:E]
+
+    for r in 1:niter
+        for e in 1:E
+            H = Hs[e]
+            for t in 1:T
+                η = r > M ? predict_h(A, H, t).^q : ones(I)
+                update_h!(X[:,t,e], W, view(H,:,t), η)
+            end
+        end
+    end
+
+    return Hs
+end
+
+function update_model_tensor!(
+    Xnew,
+    W,
+    A;
+    infer_iters=50,
+    update_A=true
+    )
+    Hs_new = infer_Hs_fixedW(Xnew, W, A; niter=infer_iters)
+
+    if update_A
+        update_A_shared!(A, Hs_new)
+    end
+
+    return Hs_new, A
+end
+
+function estimate_W_from_H(
+    X::Matrix,
+    H;
+    niter=50,
+    seed=0
+    )
+    Random.seed!(seed)
+
+    K, T = size(X)
+    I = size(H,1)
+
+    # --- PLCA scaling ---
+    X = copy(X)
+    X ./= sum(X, dims=1) .+ EPS
+    X .*= 1000.0
+
+    # --- Initialize W ---
+    W = rand(K,I)
+    normalize_columns!(W)
+
+    for _ in 1:niter
+        Γ = Array{Float64,3}(undef, K, I, T)
+
+        for t in 1:T
+            Γ[:,:,t] = responsibilities(W, H[:,t])
+        end
+
+        W = update_W(X, Γ)
+    end
+
+    return W
+end
+
+function estimate_W_from_Hs(
+    X::Array{<:Real,3},
+    Hs;
+    niter=50,
+    seed=0
+    )
+    Random.seed!(seed)
+
+    K, T, E = size(X)
+    I = size(Hs[1],1)
+
+    # --- PLCA scaling ---
+    X = copy(X)
+    for e in 1:E, t in 1:T
+        X[:,t,e] ./= sum(X[:,t,e]) + EPS
+        X[:,t,e] .*= 1000
+    end
+
+    # --- Initialize W ---
+    W = rand(K,I)
+    normalize_columns!(W)
+
+    for _ in 1:niter
+        W = update_W_tensor(X, Hs, W)
+    end
+
+    return W
+end
